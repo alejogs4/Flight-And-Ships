f8d18b86579c1fc9a4e53f45caf496903655a7b0
alejogs4 <alejogs4@gmail.com>
2017-05-09 21:48:13 -0500

    Revert "Animacion de la explosion"
    This reverts commit 2e42bf4ddc5e6178f0c69249f25e5d2f248f2391.

--- SegundaPantalla.java
+++ SegundaPantalla.java
@@ -17,7 +17,7 @@ public class SegundaPantalla extends Mundos
     public SegundaPantalla()
     {    
         prepare();   
-        barco.setPuntaje(65);
+        barco.setPuntaje(45);
         avion.setMundo(1);
         avion2.setMundo(1);
         avion3.setMundo(1);
